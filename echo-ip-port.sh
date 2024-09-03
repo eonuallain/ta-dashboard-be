9060459a9b2ab376edaf7b8ccb88ff32401e7bef
eonuallain <dev.eonuallain@gmail.com>
2024-09-03 22:43:43 +0100

    rename to TA text analysis

--- echo-ip-port.sh
+++ echo-ip-port.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services rot13-dashboard)
+export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services ta-dashboard)
 export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
 echo http://$NODE_IP:$NODE_PORT
