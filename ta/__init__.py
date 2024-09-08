2b22953e8d797493a5a23adb44996493ac700fa5
eonuallain <dev.eonuallain@gmail.com>
2024-09-08 17:33:32 +0100

    fix pylint issues

--- ta/__init__.py
+++ ta/__init__.py
@@ -1,7 +1,8 @@
+"""Text analysis"""
 
 from flask import Flask
+
 app = Flask(__name__)
 
-
+#pylint: disable=wrong-import-position
 import ta.views
-
--- ta/views.py
+++ ta/views.py
@@ -1,15 +1,4 @@
-#import os
-
-#from flask import render_template
-
-#@app.route("/")
-#def home():
-#  host = os.environ.get('HOSTNAME')
-#  print(f'host: {host}')
-#  return render_template("index.html", host=host)
-
-
-
+"""Text analysis views"""
 
 import os
 
@@ -18,6 +7,7 @@ from ta import app
 
 @app.route('/')
 def index():
-  host = os.environ.get('HOSTNAME')
-  print(f'host: {host}')
-  return render_template("index.html", host=host)
+    """The index page handler"""
+    host = os.environ.get('HOSTNAME')
+    print(f'host: {host}')
+    return render_template("index.html", host=host)
